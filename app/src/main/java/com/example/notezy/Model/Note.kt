c226b422ac4b937192c1d22f05b4359358bc6032
Himanshu Kumawat <013himanshu@gmail.com>
2020-12-16 01:19:38 +0530

    implemented safe args

--- app/src/main/java/com/example/notezy/Model/Note.kt
+++ app/src/main/java/com/example/notezy/Model/Note.kt
@@ -1,9 +1,12 @@
 package com.example.notezy.Model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "note_table")
 data class Note(
     @ColumnInfo(name = "title")
@@ -11,7 +14,8 @@ data class Note(
 
     @ColumnInfo(name = "note")
     val note: String
-) {
+): Parcelable
+{
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0L
 }
