5e8e0cd66c10abd81eb00077bd00548f20f65511
Himanshu Kumawat <013himanshu@gmail.com>
2020-12-19 14:23:02 +0530

    updated deprecated handler method

--- app/src/main/java/com/example/notezy/Splashscreen.kt
+++ app/src/main/java/com/example/notezy/Splashscreen.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        Handler().postDelayed({
+        Handler(Looper.myLooper()!!).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 1000)
